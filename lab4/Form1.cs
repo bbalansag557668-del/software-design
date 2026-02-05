1336f3ac8b92a51cd0f0e101a161f8c405ac7419
CpE Lab_BE 214 <CpE Lab_BE 214@BE216-PC04>
2026-02-05 08:27:42 +0800

    Hello

--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -6,5 +6,19 @@ namespace lab4
 		{
 			InitializeComponent();
 		}
-	}
+		[TestMethod]
+        public void TestBookCreation()
+        {
+			Book book = new Book { Title = "C# Programming", Author = "John Smith" };
+            Assert.AreEqual("C# Programming", book.Title);
+            Assert.AreEqual("John Smith", book.Author);
+        }
+		[TestMethod]
+		public void TestMagazineCreation()
+		{
+            Magazine magazine = new Magazine { Title = "Tech Weekly", IssueNumber = 12 };
+            Assert.AreEqual("Tech Weekly", magazine.Title);
+            Assert.AreEqual(12, magazine.IssueNumber);
+        }
+    }
 }
